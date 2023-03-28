574244fcdb975d7936b8971f54fee231eece6bda
Andres Espitia <duvaspiti@gmail.com>
2023-03-27 23:38:15 -0500

    Revert "Revert "optimizacion de directorios""
    This reverts commit 3b858836792080fab2695d4d7f5ccc60148fbfb7.

--- src/public/usuarios/menu/JS/index.js
+++
@@ -1,5 +0,0 @@
-let per=document.getElementById("per")
-per.addEventListener("click", function () {
-    console.log("aqui");
-    per.style.backgroundImage=" url(./usuarios/menu/icons/atras2.png)";
-})
