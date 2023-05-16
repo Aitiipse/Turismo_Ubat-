004a7ebb8ad1de7cb61ede5d3c53ee01189ee6a0
Andres Espitia <duvaspiti@gmail.com>
2023-05-15 23:42:48 -0500

    verificacion del espacio del street view

--- src/public/js/lista.js
+++ src/public/js/lista.js
@@ -42,17 +42,32 @@ window.addEventListener("DOMContentLoaded", async (e) => {
 
 
 
-          const html = actual.street;
+          const regex = /^<iframe\s+src="https:\/\/www\.google\.com\/maps\/embed\?.*?"\s+width="\d+"\s+height="\d+"\s+style="border:0;"\s+allowfullscreen=""\s+loading="lazy"\s+referrerpolicy="no-referrer-when-downgrade"><\/iframe>$/
+
+          const str = actual.street;
+
+          if (regex.test(str)) {
+
+            const html = actual.street;
           const urlRegex = /src="([^"]*)"/; // Expresión regular que busca el texto dentro de src=""
           const urlMatch = html.match(urlRegex); // Ejecuta la expresión regular en el código HTML y devuelve un array con la coincidencia
 
           if (urlMatch && urlMatch[1]) { // Comprueba que haya una coincidencia y que exista el índice 1 del array (que contiene la URL)
             const url = urlMatch[1]; // Asigna la URL a una variable
             console.log(url); // Imprime la URL en la consola
-          
-          sectionlista.querySelector("#street").src = url;
+
+            sectionlista.querySelector("#street").src = url;
+            sectionlista.querySelector(".streetviewlis").style.display = "block";
           }
 
+            console.log('La cadena cumple con la estructura de un iframe de Google Maps.');
+          } else {
+            sectionlista.querySelector(".streetviewlis").style.display = "none";
+
+          }
+
+
+          
         } catch (error) {
           console.log(error);
           console.log("no llega");
